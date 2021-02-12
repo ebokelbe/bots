4757df7e8e7062b0501fc2f5523772bb248082e2
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-12 12:13:03 -0500

    stacking boxes with loop

--- generate.py
+++ generate.py
@@ -8,6 +8,8 @@ y = 0
 z = 0.5
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1], size=[length,width,height])
+
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z+i], size=[length,width,height])
+
 pyrosim.End()
