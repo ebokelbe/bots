6b96bea0adaaf035905c6502a876ced7f48865c2
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-12 10:27:49 -0500

    added plane.urdf from pybullet_data as floor

--- simulate.py
+++ simulate.py
@@ -1,10 +1,12 @@
+import pybullet_data
 import pybullet as p
 import time as t
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.disconnect
 
 p.setGravity(0,0,-9.8)
-
+p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
 for i in range(1000):
