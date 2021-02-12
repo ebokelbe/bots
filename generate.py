e132351ee0730b2f2019f3420d52c5426a5681c3
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-12 18:13:40 -0500

    setup torso and two legs & joints

--- generate.py
+++ generate.py
@@ -18,9 +18,12 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size=[length,width,height])
-    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position="0.5 0 1.0")
-    pyrosim.Send_Cube(name="Leg", pos=[0.5,0,0.5], size=[length,width,height])
+    pyrosim.Send_Cube(name="Torso", pos=[1,0,1.5], size=[length,width,height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position="0.5 0 1.0")
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[length,width,height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position="1.5 0 1.0")
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[length,width,height])
+
 
     pyrosim.End()
 
