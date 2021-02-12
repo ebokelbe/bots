ae5d008f78f6d57f90ae173b283cdc2517f58ceb
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-12 12:08:41 -0500

    adjusted starting position of second box

--- generate.py
+++ generate.py
@@ -9,5 +9,5 @@ z = 0.5
 
 pyrosim.Start_SDF("boxes.sdf")
 pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x,y,z], size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1], size=[length,width,height])
 pyrosim.End()
