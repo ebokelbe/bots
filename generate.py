b8a918271e3f8244804152d6a07bc8278ee44fa4
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-12 15:40:19 -0500

    code for creating 6 x 6 array of towers

--- generate.py
+++ generate.py
@@ -1,19 +1,22 @@
 import pyrosim.pyrosim as pyrosim
-length = 1
-width = 1
-height = 1
-
-x = 0
-y = 0
-z = 0.5
 
 pyrosim.Start_SDF("boxes.sdf")
 
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-    z = z + height
-    length = 0.9 * length
-    width = 0.9 * width
-    height = 0.9 * height
+for i in range(6):
+    x = i
+
+    for j in range(6):
+        y = j
+        z = 0.5
+        length = 1
+        width = 1
+        height = 1
+
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+            z = z + height
+            length = 0.9 * length
+            width = 0.9 * width
+            height = 0.9 * height
 
 pyrosim.End()
