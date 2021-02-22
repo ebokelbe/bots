94476712b35b59679f89ca87af97cd5c02296b0e
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-22 16:40:39 -0500

    added sensors for front leg and saved to npy

--- simulate.py
+++ simulate.py
@@ -15,10 +15,12 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate("body.urdf")
 backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 
 for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     t.sleep(1/60)
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
-print(backLegSensorValues)
+np.save('data/frontLegSensorValues.npy', frontLegSensorValues)
