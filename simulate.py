a66cfb799cf9da8caf0a0833d769067b631bac97
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-22 15:49:46 -0500

    added back leg touch sensor

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet_data
 import pybullet as p
 import time as t
+import pyrosim.pyrosim as pyrosim
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.disconnect
@@ -10,6 +11,9 @@ p.loadURDF("plane.urdf")
 p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate("body.urdf")
+
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     t.sleep(1/60)
