db6559d0295b94bfeffc4a518bb0343f0e32ae77
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-22 16:47:03 -0500

    added front leg data to plot

--- analyze.py
+++ analyze.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load('data/backLegSensorValues.npy')
+frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
 
-plt.plot(backLegSensorValues)
+plt.plot(backLegSensorValues, linewidth=3, label='Back Leg')
+plt.plot(frontLegSensorValues, label='Front Leg')
+plt.legend()
 plt.show()
