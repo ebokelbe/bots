e4bcb1714076f1a6b74e1a26826a1ab723677033
ebokelbe <ethan.bokelberg@gmail.com>
2021-02-22 16:20:20 -0500

    added vector to store sensor values

--- simulate.py
+++ simulate.py
@@ -2,6 +2,8 @@ import pybullet_data
 import pybullet as p
 import time as t
 import pyrosim.pyrosim as pyrosim
+import numpy as np
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.disconnect
@@ -12,9 +14,11 @@ p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate("body.urdf")
+backLegSensorValues = np.zeros(1000)
 
 for i in range(1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     t.sleep(1/60)
+
+print(backLegSensorValues)
