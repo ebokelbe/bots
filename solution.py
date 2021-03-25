a951e7e1df774d2a75289f5b6181abb92325104a
ebokelbe <ethan.bokelberg@gmail.com>
2021-03-25 11:36:56 -0400

    remove print/exit

--- solution.py
+++ solution.py
@@ -3,7 +3,4 @@ import numpy as np
 class SOLUTION:
     def __init__(self):
         self.weights = np.random.rand(2, 3)
-        print(self.weights)
         self.weights = self.weights * 2 - 1
-        print(self.weights)
-        exit()
