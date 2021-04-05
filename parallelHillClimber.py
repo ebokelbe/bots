322f201ef826f910445c17e8b2aae187e8d8e0fb
ebokelbe <ethan.bokelberg@gmail.com>
2021-04-05 19:33:22 -0400

    update spawn method

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -1,9 +1,12 @@
 from solution import SOLUTION
 import constants as c
 import copy
+import os
 
 class PARALLEL_HILL_CLIMBER:
     def __init__(self):
+        os.system("del brain*.nndf")
+        os.system("del fitness*.txt")
         self.nextAvailableID = 0
         self.parents = {}
         for i in range(c.populationSize):
@@ -15,21 +18,22 @@ class PARALLEL_HILL_CLIMBER:
             self.parents[parent].Start_Simulation("DIRECT")
         for parent in self.parents:
             self.parents[parent].Wait_For_Simulation_To_End()
-        # self.parent.Evaluate("GUI")
-        # for currentGeneration in range(c.numberOfGenerations):
-        #     self.Evolve_For_One_Generation()
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
         self.Spawn()
-        self.Mutate()
-        self.child.Evaluate("DIRECT")
-        self.Print()
-        self.Select()
+        # self.Mutate()
+        # self.child.Evaluate("DIRECT")
+        # self.Print()
+        # self.Select()
 
     def Spawn(self):
-        self.child = copy.deepcopy(self.parent)
-        self.child.Set_ID(self.nextAvailableID)
-        self.nextAvailableID += 1
+        self.children = {}
+        for i in self.parents:
+            self.children[i] = copy.deepcopy(self.parents[i])
+            self.children[i].Set_ID(self.nextAvailableID)
+            self.nextAvailableID += 1
 
     def Mutate(self):
         self.child.Mutate()
