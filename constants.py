0b8d5416ba36928ac815f743af22853910478ab1
ebokelbe <ethan.bokelberg@gmail.com>
2021-04-12 18:08:03 -0400

    added constants to constrain the movement

--- constants.py
+++ constants.py
@@ -6,9 +6,11 @@ backPhaseOffset = 0
 
 x = np.linspace(-np.pi, np.pi, 1000)
 
-numberOfGenerations = 1
+numberOfGenerations = 15
 
-populationSize = 1
+populationSize = 15
 
-numSensorNeurons = 3
-numMotorNeurons = 2
+numSensorNeurons = 4
+numMotorNeurons = 8
+
+motorJointRange = 0.2
